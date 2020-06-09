0feebcc00e44d5b1649e5288b5034da0f18fbc30
Bourek, Daniel <bouredan@fel.cvut.cz>
2020-06-09 02:19:56 +0200

    update javadoc

--- doc/package-search-index.js
+++ doc/package-search-index.js
@@ -1,1 +1,22 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"m":"cz.cvut.fel.bouredan.chess","l":"cz.cvut.fel.bouredan.chess"},{"m":"cz.cvut.fel.bouredan.chess","l":"cz.cvut.fel.bouredan.chess.common"},{"m":"cz.cvut.fel.bouredan.chess","l":"cz.cvut.fel.bouredan.chess.game"},{"m":"cz.cvut.fel.bouredan.chess","l":"cz.cvut.fel.bouredan.chess.game.board"},{"m":"cz.cvut.fel.bouredan.chess","l":"cz.cvut.fel.bouredan.chess.game.io"},{"m":"cz.cvut.fel.bouredan.chess","l":"cz.cvut.fel.bouredan.chess.game.piece"},{"m":"cz.cvut.fel.bouredan.chess","l":"cz.cvut.fel.bouredan.chess.gui"},{"m":"cz.cvut.fel.bouredan.chess","l":"cz.cvut.fel.bouredan.chess.gui.assets"},{"m":"cz.cvut.fel.bouredan.chess","l":"cz.cvut.fel.bouredan.chess.gui.board"},{"m":"cz.cvut.fel.bouredan.chess","l":"cz.cvut.fel.bouredan.chess.gui.pieces"}]
+packageSearchIndex = [{"l": "All Packages", "url": "allpackages-index.html"}, {
+    "m": "cz.cvut.fel.bouredan.chess",
+    "l": "cz.cvut.fel.bouredan.chess"
+}, {"m": "cz.cvut.fel.bouredan.chess", "l": "cz.cvut.fel.bouredan.chess.common"}, {
+    "m": "cz.cvut.fel.bouredan.chess",
+    "l": "cz.cvut.fel.bouredan.chess.game"
+}, {
+    "m": "cz.cvut.fel.bouredan.chess",
+    "l": "cz.cvut.fel.bouredan.chess.game.board"
+}, {"m": "cz.cvut.fel.bouredan.chess", "l": "cz.cvut.fel.bouredan.chess.game.io"}, {
+    "m": "cz.cvut.fel.bouredan.chess",
+    "l": "cz.cvut.fel.bouredan.chess.game.piece"
+}, {
+    "m": "cz.cvut.fel.bouredan.chess",
+    "l": "cz.cvut.fel.bouredan.chess.game.player"
+}, {"m": "cz.cvut.fel.bouredan.chess", "l": "cz.cvut.fel.bouredan.chess.gui"}, {
+    "m": "cz.cvut.fel.bouredan.chess",
+    "l": "cz.cvut.fel.bouredan.chess.gui.assets"
+}, {"m": "cz.cvut.fel.bouredan.chess", "l": "cz.cvut.fel.bouredan.chess.gui.board"}, {
+    "m": "cz.cvut.fel.bouredan.chess",
+    "l": "cz.cvut.fel.bouredan.chess.gui.pieces"
+}]
